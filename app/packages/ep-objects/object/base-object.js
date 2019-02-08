eecfe380b9ce36cfc005bd8576ee503038ad74e4
Chung-Sheng Wu <samsam2310@gmail.com>
2019-02-08 22:47:24 +0800

    delete old code

--- app/packages/ep-objects/object/base-object.js
+++
@@ -1,9 +0,0 @@
-// Base Object
-
-BaseObject = function(timeTag){
-  this.timeTag = timeTag;
-};
-
-// BaseObject.prototype.render = function(ctx){
-//   ;
-// }
--- app/server/main.js
+++
@@ -1,5 +0,0 @@
-// main server code
-
-Meteor.startup(function () {
-  // code to run on server at startup
-});
